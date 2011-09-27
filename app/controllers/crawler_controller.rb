5806b18d12541eb2a0f75e0a7eca1640bfb01bd4
vood <vood@vood-VirtualBox.(none)>
2011-09-27 17:50:23 +0300

    latest changes. working version

--- app/controllers/crawler_controller.rb
+++ app/controllers/crawler_controller.rb
@@ -6,15 +6,15 @@ class CrawlerController < ApplicationController
         'http://it.tut.by/rss.xml' => '#article_body',
         'http://www.ej.by/news/it/it.rss' => 'body',
         'http://feeds.feedburner.com/providers_by' => 'body',
-        'http://news.21.by/pub/rss/all.rss' => 'body'
+        'http://news.21.by/pub/rss/all.rss' => '.editor .sizeable'
     }
 
-    regex = Category.all(:include => :tags).collect { |c| [c.id, Regexp.union(c.tag_list)] }
-
-    Feedzirra::Feed.fetch_and_parse(feeds.keys).values.each do |feed|
-      feed.entries.each do |entry|
+    regex = Category.all(:include => :tags).collect { |c| [c.id, Regexp.union(c.tag_list)] } 
+   debugger 
+    feeds.each_pair do |url, selector| 
+      Feedzirra::Feed.fetch_and_parse(url).entries.each do |entry|
         regex.each do |r|
-          create_or_update_post(entry, r[0], feeds[feed.url]) if  entry_matches(entry, r[1])
+          create_or_update_post(entry, r[0], selector) if entry_matches(entry, r[1])
         end
       end
     end
