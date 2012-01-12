629c300b4a184eccb4b3226845f83fe1fec9131c
Artem Vysotsky <artem.vysotsky@gmail.com>
2012-01-12 04:15:53 +0300

    latest changes plus tests

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,7 @@ module Belta
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.compile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
