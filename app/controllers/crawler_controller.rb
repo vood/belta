ad213b4fec47e6498cfd9c4082a60c14a19f0af7
Artem Vysotsky <artem.vysotsky@gmail.com>
2012-01-13 01:39:04 +0300

    latest changes plus tests

--- app/controllers/crawler_controller.rb
+++ app/controllers/crawler_controller.rb
@@ -3,15 +3,13 @@ require 'open-uri'
 class CrawlerController < ApplicationController
   def index
     @feeds = Feed.all_by_url
-    Feedzirra::Feed.fetch_and_parse(@feeds.keys).each do |feed|
-      parse_feed_entry feed
-    end
+    Feedzirra::Feed.fetch_and_parse(@feeds.keys, :on_success => lambda{ |url, feed| parse_feed_entry(url, feed)  } )
     redirect_back :notice => "Новости успешно обновлены"
   end
 
-  def parse_feed_entry rssfeed
-    feed = @feeds[rssfeed[0]]
-    rssfeed[1].entries.each do |entry|
+  def parse_feed_entry url, rssfeed
+    feed = @feeds[url]
+    rssfeed.entries.each do |entry|
       body = open(entry.url).read
       category = feed.category || Category.matched(entry.title, body)
       Post.create_or_update_by_source(
