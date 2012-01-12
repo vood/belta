07233e668fa231378720dcde1c17515e9b82adf5
Artem Vysotsky <artem.vysotsky@gmail.com>
2012-01-13 01:18:30 +0300

    latest changes plus tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def redirect_back notice
+    redirect_to :back, notice
+  rescue ActionController::RedirectBackError
+    redirect_to root_path, notice
+  end
+
 end
--- app/controllers/crawler_controller.rb
+++ app/controllers/crawler_controller.rb
@@ -3,14 +3,16 @@ require 'open-uri'
 class CrawlerController < ApplicationController
   def index
     @feeds = Feed.all_by_url
-    Feedzirra::Feed.fetch_and_parse(@feeds.keys, :on_success => :parse_feed_entry)
-    redirect_to :back, :notice => "Новости успешно обновлены" if request.env['HTTP_REFERER']
+    Feedzirra::Feed.fetch_and_parse(@feeds.keys).each do |feed|
+      parse_feed_entry feed
+    end
+    redirect_back :notice => "Новости успешно обновлены"
   end
 
-  def parse_feed_entry url, rssfeed
-    feed = @feeds[url]
-    rssfeed.entries.each do |entry|
-      body = open(entry.url)
+  def parse_feed_entry rssfeed
+    feed = @feeds[rssfeed[0]]
+    rssfeed[1].entries.each do |entry|
+      body = open(entry.url).read
       category = feed.category || Category.matched(entry.title, body)
       Post.create_or_update_by_source(
           :source => entry.url,
