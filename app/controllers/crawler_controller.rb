b951cce0035819eaec99a3992bf3d8f20b92134c
Artem Vysotsky <artem.vysotsky@gmail.com>
2012-01-13 00:40:03 +0300

    latest changes plus tests

--- app/controllers/crawler_controller.rb
+++ app/controllers/crawler_controller.rb
@@ -2,22 +2,23 @@
 require 'open-uri'
 class CrawlerController < ApplicationController
   def index
-    feeds = Feed.all(:include => :category)
-
-    feeds.each do |feed|
-      Feedzirra::Feed.fetch_and_parse(feed.url).entries.each do |entry|
-        body = open(entry.url)
-        category = feed.category || Category.matched(entry.title, body)
-        Post.create_or_update_by_source(
-            :source => entry.url,
-            :title => entry.title,
-            :body => Parser.parse(body, feed.selector, feed.selector_blacklist),
-            :published_at => entry.published,
-            :categories => [category]
-        ) if category
-      end
-    end
-
+    @feeds = Feed.all_by_url
+    Feedzirra::Feed.fetch_and_parse(@feeds.keys, :on_success => :parse_feed_entry)
     redirect_to :back, :notice => "Новости успешно обновлены" if request.env['HTTP_REFERER']
+  end
+
+  def parse_feed_entry url, rssfeed
+    feed = @feeds[url]
+    rssfeed.entries.each do |entry|
+      body = open(entry.url)
+      category = feed.category || Category.matched(entry.title, body)
+      Post.create_or_update_by_source(
+          :source => entry.url,
+          :title => entry.title,
+          :body => Parser.parse(body, feed.selector, feed.selector_blacklist),
+          :published_at => entry.published,
+          :categories => [category]
+      ) if category
+    end
   end
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,11 @@
 class Feed < ActiveRecord::Base
   belongs_to :category
+
+  def all_by_url
+    hash = {}
+    all(:include => :category).each do |feed|
+      hash[feed.url] = feed
+    end
+    hash
+  end
 end
