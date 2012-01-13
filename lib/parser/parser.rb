245480bfaec00c256f7ba699162ac6ca8a289a38
Artem Vysotsky <artem.vysotsky@gmail.com>
2012-01-14 01:22:08 +0300

    fix title

--- lib/parser/parser.rb
+++ lib/parser/parser.rb
@@ -10,7 +10,7 @@ class Parser
     if blacklist.length > 1
       nodes.css(blacklist).remove
     end
-    Sanitize.clean(nodes.to_html, Sanitize::Config::RESTRICTED.merge(:remove_contents => %w[script])).strip
+    Sanitize.clean(nodes.to_html, Sanitize::Config::RESTRICTED.merge(:remove_contents => %w[script img])).strip
   end
 
 end
