a7a65870d1ef05fe0dbf8d6ce6f62b0e33eb3962
Artem Vysotsky <artem.vysotsky@gmail.com>
2012-01-13 18:40:55 +0300

    more informative message

--- app/controllers/crawler_controller.rb
+++ app/controllers/crawler_controller.rb
@@ -3,8 +3,10 @@ require 'open-uri'
 class CrawlerController < ApplicationController
   def index
     @feeds = Feed.all_by_url
+    @updated = 0
+    @created = 0
     Feedzirra::Feed.fetch_and_parse(@feeds.keys, :on_success => lambda { |url, feed| parse_feed_entry(url, feed) if feed })
-    redirect_back :notice => "Новости успешно обновлены"
+    redirect_back :notice => "Новости успешно обновлены. %d добавлено, %d обновлено." % [@created, @updated]
   end
 
   def parse_feed_entry url, rssfeed
@@ -14,12 +16,14 @@ class CrawlerController < ApplicationController
       body = Parser.parse(body, feed.selector, feed.selector_blacklist)
       category = feed.category || Category.matched(entry.title, strip_tags(body))
       if category
-        Post.create_or_update_by_source(
+        post = Post.create_or_update_by_source(
             :source => entry.url,
             :title => entry.title,
             :body => body,
             :published_at => entry.published,
             :categories => [category])
+        post.was_new ? @created += 1 : @updated +=1
+
       end
     end
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,8 @@ class Post < ActiveRecord::Base
 
   TITLES = {'Позитивная' => 1, 'Нейтральная' => 0, 'Негативная' => -1}.invert
 
+  @was_new = true
+
   belongs_to :theme
   has_many :category_posts
   has_many :categories, :through => :category_posts
@@ -24,6 +26,7 @@ class Post < ActiveRecord::Base
   def self.create_or_update_by_source params
     related_post = find_by_title(params[:title])
     post = find_or_initialize_by_source(params[:source])
+    post.was_new = post.new_record?
     post.update_attributes(params)
     related_post.related.push(post) if related_post && related_post != post
     post
