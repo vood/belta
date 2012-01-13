832f52d9a67a6add1ab287e8f5e675a7aa3abe7e
Artem Vysotsky <artem.vysotsky@gmail.com>
2012-01-13 23:17:56 +0300

    more tests

--- app/controllers/crawler_controller.rb
+++ app/controllers/crawler_controller.rb
@@ -5,25 +5,30 @@ class CrawlerController < ApplicationController
     @feeds = Feed.all_by_url
     @updated = 0
     @created = 0
+    @errors  = 0
     Feedzirra::Feed.fetch_and_parse(@feeds.keys, :on_success => lambda { |url, feed| parse_feed_entry(url, feed) if feed })
-    redirect_back :notice => "Новости успешно обновлены. %d добавлено, %d обновлено." % [@created, @updated]
+    redirect_back :notice => "Новости успешно обновлены. %d добавлено, %d обновлено, ошибок %d." % [@created, @updated, @errors]
   end
 
   def parse_feed_entry url, rssfeed
     feed = @feeds[url]
     rssfeed.entries.each do |entry|
-      body = open(entry.url).read
-      body = Parser.parse(body, feed.selector, feed.selector_blacklist)
-      category = feed.category || Category.matched(entry.title, strip_tags(body))
-      if category
-        post = Post.create_or_update_by_source(
-            :source => entry.url,
-            :title => entry.title,
-            :body => body,
-            :published_at => entry.published,
-            :categories => [category])
-        post.was_new ? @created += 1 : @updated +=1
-
+      begin
+        body = open(entry.url).read
+        body = Parser.parse(body, feed.selector, feed.selector_blacklist)
+        category = feed.category || Category.matched(entry.title, strip_tags(body))
+        if category
+          post = Post.create_or_update_by_source(
+              :source => entry.url,
+              :title => entry.title,
+              :body => body,
+              :published_at => entry.published,
+              :categories => [category])
+          post.was_new ? @created += 1 : @updated +=1
+        end
+      rescue Exception => e
+        logger.error('Crawler: ' + e.to_s)
+        @errors += 1
       end
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120113124805) do
+ActiveRecord::Schema.define(:version => 20120113145520) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(:version => 20120113124805) do
   create_table "feeds", :force => true do |t|
     t.string   "url"
     t.string   "selector"
-    t.boolean  "xpath_selector"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "selector_blacklist"
